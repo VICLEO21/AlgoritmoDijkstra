bde53beb60449ac0fd0e8f59bdc47bdc65279dc3
Victor Garcia <victor.g@uteca.edu.mx>
2024-03-21 08:31:25 -0600

    Avances clase

--- main.py
+++ main.py
@@ -41,16 +41,41 @@ grafoTest.addArista('E','D',8)
 origenG = 'A'
 destinoG = 'E'
 path={}
+visitados = []
+
+visitados.append (origenG)
 path[origenG]={'-':0}
 
 llaves = grafoTest.Aristas[origenG].keys()
 print(llaves)
-verticeAct = 'B'
 
 for i in llaves:
     path[i]={origenG : grafoTest.Aristas[origenG][i]}
 
 
+verticeAct = 'B'
+visitados.append (verticeAct)
+llaves = grafoTest.Aristas [ verticeAct ].keys()
+print(llaves)
 
+for i in llaves:
+    if i not in visitados:
+        if i not in path: path[i] = {}
+        llave = list (path [verticeAct].keys())
+        acumulado = path [verticeAct][llave[0]]
+            
+        path[i].update ( {verticeAct : grafoTest.Aristas[verticeAct][i] + acumulado})
+
+        # reviso si tiene mas de 2 llaves en un allave del path 
+        if len(path[i]) == 2 :
+            kiss = list (path[i].keys() )
+            # reviso cual es mayor para borralo (del) y quedarme con el menor
+            if kiss[0] > kiss [1]:
+                del (path[i][kiss[0]])
+            elif kiss[1] > kiss[0]:
+                del (path[i][kiss[1]])
+            pass
+
+print (visitados)
 print(path)      
-print(grafoTest)
+#print(grafoTest)
